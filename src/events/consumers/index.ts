aabbe3ce771725532d2ddde05164b3003b91fee1
JeremyG11 <gatwech3211@gmail.com>
2025-07-03 13:00:58 +0300

    Switching to the same site lax on moving to same domain for both the frontent and API . .

--- src/events/consumers/index.ts
+++ src/events/consumers/index.ts
@@ -1,14 +1,11 @@
 import { config } from "@/config/env";
 import { loggerInstance } from "@payvo/logger";
-import { setupRegistrationConsumer } from "./registerEventConsumer";
 
 const logger = loggerInstance(config.serviceName);
 
 export const startConsumers = async (): Promise<void> => {
   try {
     logger.info(`Starting Kafka consumers for ${config.serviceName}`);
-
-    await setupRegistrationConsumer();
 
     logger.info(
       `All Kafka consumers started successfully for ${config.serviceName}`
